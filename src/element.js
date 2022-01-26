e67470fbc80f871db0f8d7ddf7f30c1f2d0558a4
nutella <nutell@4ka.com>
2022-01-26 13:34:27 -0500

    feat: implemented the createElement function

---
+++ src/element.js
@@ -0,0 +1,23 @@
+export const createElement = (type, props, ...children) => {
+  return {
+    type,
+    props: {
+      ...props,
+      children: children.map(child => 
+        typeof child === "object" 
+          ? child
+          : createTextElement(child)
+      )
+    }
+  };
+}
+
+const createTextElement = (text) => {
+  return {
+    type: "TEXT_ELEMENT",
+    props: {
+      nodeValue: text,
+      children: []
+    }
+  }
+}
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import { createElement } from "./element";
+
+export const SimpleReact = {
+  createElement
+}
